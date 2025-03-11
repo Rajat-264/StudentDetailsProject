106a53c6b95c9bbc5e2b0dc7d3b559389224522e
KrantiSambhav7 <krantisambhav7@gmail.com>
2025-03-11 09:50:38 +0530

    Navbar added

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -2,16 +2,33 @@ import {Link} from 'react-router-dom';
 import './navbar.css';
 const Navbar = () => {
     return (
-        <div className="navbar">
-            <div className="logo">
-            <Link to="/" style={{textDecoration:"none"}}><span className="logo-entry">ActiveCampus</span></Link>
-            </div>
-            <div className="links">
-                <Link to="/" style={{textDecoration:"none"}}><span className="entries">Home</span></Link>
-                <Link to="/Profile" style={{textDecoration:"none"}}><span className="entries">Profile</span></Link>
-                <Link to="/Help" style={{textDecoration:"none"}}><span className="entries">Help</span></Link>
-            </div>
+        <div className="navbar border border-b-2 bg-black ">
+        <div className="logo">
+            <Link to="/" style={{ textDecoration: "none" }}>
+                <span className="logo-entry">EduTech</span>
+            </Link>
         </div>
+        <div className="links">
+            <Link to="/" style={{ textDecoration: "none" }}>
+                <div className="flex justify-between items-center gap-1.5">
+                <div className="h-[5px] w-[5px] rounded-full bg-green-600 shadow-md shadow-green-700 transition-all duration-200 hover:bg-green-900"></div>
+                    <span className="entries hover:">Home</span>
+                </div>
+            </Link>
+            <Link to="/Profile" style={{ textDecoration: "none" }}>
+            <div className="flex justify-between items-center gap-1.5">
+            <div className="h-[5px] w-[5px] rounded-full bg-green-600 shadow-md shadow-green-700 transition-all duration-200 hover:bg-green-900"></div>
+                    <span className="entries">Profile</span>
+                </div>
+            </Link>
+            <Link to="/Help" style={{ textDecoration: "none" }}>
+            <div className="flex justify-between items-center gap-1.5">
+            <div className="h-[5px] w-[5px] rounded-full bg-green-600 shadow-md shadow-green-700 transition-all duration-200 hover:bg-green-900"></div>
+                <span className="entries">Help</span>
+                </div>
+            </Link>
+        </div>
+    </div>
     );
 };
 
