4f543b748518e3be4b5f852268ca070863e3a283
KrantiSambhav7 <krantisambhav7@gmail.com>
2025-03-11 09:52:04 +0530

    Home.jsx added

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,15 +1,19 @@
 import {Link} from 'react-router-dom';
 import './home.css';
+import Button from '../../components/Button';
+import StudentDashboard from '../../components/StudentDashboard';
+import Announcements from '../../components/Announcements';
+
 const Home = () => {
     return (
         <div>
-            <div className="head">Student Dashboard</div>
-
+            <div className="head underline underline-offset-8">Student Dashboard</div>
             <div className="page">
                 <div className="card">
                     <div className="info">
                         <div className="text">
-                            
+                            <StudentDashboard />
+                            <Announcements />
                         </div>
                     </div>
                 </div>
